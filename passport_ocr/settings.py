28c00eff87f752d68dc8558ff3576fddcf48b9dc
satyamtiwari1004 <satyamtiwari1004@gmail.com>
2025-05-19 17:28:33 +0530

    Updated settings.py with media and static configurations

--- passport_ocr/settings.py
+++ passport_ocr/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yu11^q@5+!j7eoll%5rdny#a+^0seny8!0l^*+ywvul)ekr+_n'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-default")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definition
